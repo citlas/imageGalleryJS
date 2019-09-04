fc2b33fce16d0f79132a32c2195bef4b2d5a0ee5
Citlalli Prado <citlalli.prado@gmail.com>
2019-09-04 10:43:42 +0200

    Changed class Picture outside function

--- scripts/script.js
+++ scripts/script.js
@@ -1,66 +1,73 @@
 let startUrl= "https://api.unsplash.com/photos?client_id=5242c9920799828e90063b925625df6d53705465a6ae595a50405eec418b1a37"
 
+/*Comentarios de Albert
+
+done! --- Lo mas grave que veo de inicio sería declarar la clase Picture  dentro de una función.
+done! --- Lo que yo habría hecho sería ponerla encima de la funcion getPictures y desde dentro de la funcion map ya instancias Picture para usarla. 
+
+--- O incluso mejor sería hacer esta funcion un método de la clase Picture.
+
+done! --- Además viendo que luego generas elementos dinámicos (document.createElement), podrías haber aprovechado la clase Picture para añadir un metodo que fuera tipo buildItem()  y ahí generas los elementos y solo tendrías que llamar al método. 
+Eso teniendo en cuenta todas las promises en este caso del fetch(url), pero es solo una idea rápida que se me ocurre.
+
+class Picture {
+    constructor() {}
+    getPictures() {}
+    buildItems() {}
+    draw() {}
+}
+
+let picture = new Picture (startUrl)
+picture.getPictures();
+picture.buildItems();
+picture.draw();
+
+done --- Una tonteria de usabilidad es que (me gusta el autosearch), pero si vacías el campo de búsqueda, se queda la pagina en blanco, podrías poner el mismo default que tienes cuando cargas por primera vez la página.
+
+*/
+
+class Picture{
+  constructor(fullUrl,smallUrl,description){
+    this.fullUrl=fullUrl;
+    this.smallUrl=smallUrl;
+    this.description=description; 
+  }
+  buildItem (elementToCreate, selectClass) {
+    let itemName = document.createElement(elementToCreate)
+    itemName.className += selectClass;
+    return itemName
+ }
+}
 
 function getPictures() {
     fetch(startUrl)
-.then(function(response){
-    return response.json();
-})
-.then(function(data) {
-    //console.log(data)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)
+        let resultsToUse = data
+        if(data.hasOwnProperty("results")){ //if is going to show the search results use another data
+            resultsToUse = data.results
+        } 
+        resultsToUse.map(function(data){    
 
-    let resultsToUse = data
-    //if is going to show the search results use another data
-    if(data.hasOwnProperty("results")){
-        resultsToUse = data.results
-    } 
-    
-    resultsToUse.map(function(data){    
-
-      class Picture{
-        constructor(fullUrl,smallUrl,description){
-          this.fullUrl=fullUrl;
-          this.smallUrl=smallUrl;
-          this.description=description;
-        }
-      }
-
-      let picture = new Picture (data.urls.full,data.urls.small,data.description)
-
-
-        let imagediv = document.createElement('div')
-        imagediv.className += "eachImage";
-        // if using modal-- imagediv.onclick = function(){showModal(`${data.urls.full}`)}
-    
-        let image = document.createElement('a')
-       
-        image.className += "postImage";
-        //image.href = `${data.urls.full}`
-        //image.innerHTML += `<img src=${data.urls.small}>`
-
+        let gallery = document.querySelector("#gallery");
+        let picture = new Picture (data.urls.full,data.urls.small,data.description)
+        let imagediv = picture.buildItem('div','eachImage')
+        let image = picture.buildItem('a','postImage')
         image.href = `${picture.fullUrl}`
         image.innerHTML += `<img src=${picture.smallUrl}>`
-
-        /*for modal: change a for img, delete image.innerhtml and add
-        image.setAttribute('data-full-url',`${data.urls.full}`);
-        image.src = `${data.urls.small}`*/
-        let p = document.createElement('p')
-        p.className += "altText";
-        
-        //get description to show it on hover
-       // if(data.description){
-         // p.innerHTML = `${data.description}`;
-          
-          if(picture.description){
-            p.innerHTML = `${picture.description}`;
-
+        let p = picture.buildItem('p','altText')
+      
+        //get description to show it on hover          
+        if(picture.description){
+          p.innerHTML = `${picture.description}`;
         } else {
           p.innerHTML = `No description provided`;
-          
         }
         
-        
-        let gallery = document.querySelector("#gallery");
+        //append evertyhing
         imagediv.appendChild(image)
         imagediv.appendChild(p)
         gallery.appendChild(imagediv)
@@ -77,14 +84,21 @@ let input = document.querySelector('#termToSearch');
 input.onkeyup = searchThis;
 let timer; //created for delay the keyup search
 
+
 function searchThis() {
-clearTimeout(timer);
-   timer=setTimeout(function validate(){
-    let searchUrl = `https://api.unsplash.com/search/photos?page=1&query=${input.value}&client_id=5242c9920799828e90063b925625df6d53705465a6ae595a50405eec418b1a37`
-    startUrl = searchUrl;
-    document.querySelector('#gallery').innerHTML = "";
-    getPictures();
-   },1000);
+  if(input.value==""){
+    startUrl = "https://api.unsplash.com/photos?client_id=5242c9920799828e90063b925625df6d53705465a6ae595a50405eec418b1a37"
+    getPictures()
+  } else {
+    clearTimeout(timer);
+    timer=setTimeout(function validate(){
+     let searchUrl = `https://api.unsplash.com/search/photos?page=1&query=${input.value}&client_id=5242c9920799828e90063b925625df6d53705465a6ae595a50405eec418b1a37`
+     startUrl = searchUrl;
+     document.querySelector('#gallery').innerHTML = "";
+     getPictures();
+    },1000);
+  }
+
 }
 
 //Modal-- bad idea, full picture is way too big 
